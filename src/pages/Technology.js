21375d94ca8f27bc8366fec048639723c4af7a7d
eudisville <mondesirkoffi30@gmail.com>
2025-09-26 12:03:38 +0000

    one

--- src/pages/Technology.js
+++ src/pages/Technology.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Header from '../components/Header'
 import './styles/styles.css'
 import img1 from "./assets/digit.jpg"
+import image2 from "./assets/po.jpg"
+import image3 from "./assets/client.jpg"
+import image4 from "./assets/tics.jpg"
 import Card from '../components/Card'
 
 function Technology() {
@@ -22,19 +25,19 @@ function Technology() {
             </div>
             <div className='card-card'>
               <Card 
-                backgroundImage="" 
+                backgroundImage={image4}
                 title="La transformation digitale dans le secteur de l'assurance" 
                 content="Explorer comment nous avons aidé cette entreprise dans sa digitalisation."
               />
               <Card 
-                backgroundImage="" 
+                backgroundImage={image3}
                 title="L'expérience client 2.0 un levier de votre réussite" 
                 content="Consultez nos conseils pour améliorer le parcours de vos clients." 
               />
               <Card 
-                backgroundImage=""
-                title="Notre engagement dans le développement durable" 
-                content="Beriverse, une culture de la RSE impeccable" 
+                backgroundImage={image2}
+                title="L'impact des données dans la prise de décision" 
+                content="Découvrez comment les données peuvent transformer votre entreprise." 
               />
             </div>
           </div>
